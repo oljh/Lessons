b77c5bb6c0b2fa484dfeab6cd8f32c54a3666316
sapp <sapp@192.168.21.78>
2018-08-28 17:20:28 +0300

    dates

--- src/by/bsu/dates/DemoDateFormat.java
+++ src/by/bsu/dates/DemoDateFormat.java
@@ -1,5 +1,14 @@
 package by.bsu.dates;
 
-public class DemoDateFormat {
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
+public class DemoDateFormat {
+	public static void main(String [] args) {
+		DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.US);
+		Date d = null;
+		String str = "April 9, 2012";
+		
+	}
 }
