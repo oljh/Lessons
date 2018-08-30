9ba95c66d335659503e2eb759815def5a638d51a
sapp <sapp@192.168.21.78>
2018-08-30 17:22:27 +0300

    formatter

--- src/by/bsu/format/FormatterDemoTimeAndDate.java
+++ src/by/bsu/format/FormatterDemoTimeAndDate.java
@@ -11,6 +11,18 @@ public class FormatterDemoTimeAndDate {
 		//вывод в 12 часовом формате
 		f.format("%tr",cal);
 		System.out.println(f);
+		//полноформатный вывод данных
+		f = new Formatter();
+		f.format("%tc", cal);
+		System.out.println(f);
+//		Вывод текущего часа и даты
+		f = new Formatter();
+		f.format("%tL:%tM",cal,cal);
+		System.out.println(f);
+		//всевозможный вывод месяца
+		f = new Formatter();
+		f.format("%tB %tb %tm",cal,cal,cal);
+		System.out.println(f);
 		
 	}
 }
