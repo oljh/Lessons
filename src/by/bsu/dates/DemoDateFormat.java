9414ed57af1ea0b93ca5a3ab6632743ee40785c9
oljh <34842956+oljh@users.noreply.github.com>
2018-08-30 22:05:38 +0300

    dateFormat

--- src/by/bsu/dates/DemoDateFormat.java
+++ src/by/bsu/dates/DemoDateFormat.java
@@ -1,14 +1,28 @@
 package by.bsu.dates;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.util.Date;
 import java.util.Locale;
+
 
 public class DemoDateFormat {
 	public static void main(String [] args) {
 		DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.US);
 		Date d = null;
 		String str = "April 9, 2012";
+		try {
+			d = df.parse(str);
+			System.out.println(d);
+				
+		}catch(ParseException e) {
+			System.err.print("Error position" + e.getErrorOffset());
+		}
 		
+		df = DateFormat.getDateInstance(DateFormat.LONG, new Locale("ru","RU"));
+		System.out.println(df.format(d));
+		
+		df = DateFormat.getDateInstance(DateFormat.FULL, Locale.GERMAN);
+		System.out.println(df.format(d));
 	}
 }
