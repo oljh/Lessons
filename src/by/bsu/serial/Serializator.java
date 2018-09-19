5ce2f06185101875dbc37a4fae18a51c44e5825a
sapp <sapp@192.168.21.78>
2018-09-19 17:02:04 +0300

    fileStream

--- src/by/bsu/serial/Serializator.java
+++ src/by/bsu/serial/Serializator.java
@@ -1,5 +1,57 @@
 package by.bsu.serial;
 
-public class Serializator {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InvalidObjectException;
+import java.io.NotSerializableException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
+public class Serializator {
+	public boolean serialization(Student s, String fileName) {
+		boolean flag = false;
+		File f = new File(fileName);
+		ObjectOutputStream ostream = null;
+		try {
+			FileOutputStream fos = new FileOutputStream(f);
+			if (fos != null) {
+				ostream = new ObjectOutputStream(fos);
+				ostream.writeObject(s);// сериализация
+				flag = true;
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("Файл не может быть создан:" + e);
+		} catch (IOException e) {
+			System.err.println("" + e);
+		} finally {
+			try {
+				if (ostream != null) {
+					ostream.close();
+				}
+			} catch (IOException e) {
+				System.err.println("ошибка закрытия потока");
+			}
+		}
+		return flag;
+	}
+
+	public Student deserialization(String fileName) throws InvalidObjectException {
+		File fr = new File(fileName);
+		ObjectInputStream istream = null;
+		try {
+			FileInputStream fis = new FileInputStream(fr);
+			istream = new ObjectInputStream(fis);
+			// десериализация
+			Student st = (Student) istream.readObject();
+			return st;
+		} catch (ClassNotFoundException ce) {
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
--- src/by/bsu/serial/Student.java
+++ src/by/bsu/serial/Student.java
@@ -8,10 +8,14 @@ public class Student implements Serializable{
 	private int id;
 	private transient String password;
 	private static final long serialVersionUID = 1L;
-	/* смысл поля serialVersionUID для класса будет объяснен ниже */
+	/* смысл поля serialVersionUID для класса будет объяснен ниже*/
 		public Student (String nameOfFaculty, String name, String password) {
 			faculty = nameOfFaculty;
 			this.name = name;
-			this.id
+			this.id = id;
+			this.password = password;
+		}
+		public String toString() {
+			return "\nfaculty"+faculty +"\nName" + name + "\nID"+ id + "\npassword" + password;
 		}
 }
