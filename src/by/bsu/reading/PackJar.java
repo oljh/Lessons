9671a2b54cf3dafd723b5b118684edef75396e16
sapp <sapp@192.168.21.78>
2018-10-12 14:41:14 +0300

    pack

--- src/by/bsu/reading/PackJar.java
+++ src/by/bsu/reading/PackJar.java
@@ -1,16 +1,59 @@
 package by.bsu.reading;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+import java.util.zip.Deflater;
 
 public class PackJar {
 	private String jarFileName;
 	public final int BUFFER = 2_048;
+
 	public PackJar(String dirName) throws FileNotFoundException {
-		//получение списка имен файлов в директории
+		// получение списка имен файлов в директории
 		File dir = new File(dirName);
 		if (!dir.exists() || !dir.isDirectory()) {
-			
+			throw new FileNotFoundException(dir + " not found");
+		}
+
+		File[] files = dir.listFiles();
+		ArrayList<String> listFilesToJar = new ArrayList<>();
+		for (int i = 0; i < files.length; i++) {
+			if (!files[i].isDirectory()) {
+				listFilesToJar.add(files[i].getPath());
+			}
+		}
+		String[] temp = {};
+		String[] filesToJar = listFilesToJar.toArray(temp);
+
+		// архивирование
+		try (FileOutputStream fos = new FileOutputStream(jarFileName); JarOutputStream jos = new JarOutputStream(fos)) {
+			byte[] buffer = new byte[BUFFER];
+			// метод сжатия
+			jos.setLevel(Deflater.DEFAULT_COMPRESSION);
+			for (int i = 0; i < filesToJar.length; i++) {
+				jos.putNextEntry(new JarEntry(filesToJar[i]));
+				try (FileInputStream in = new FileInputStream(filesToJar[i])) {
+					int len;
+					while ((len = in.read(buffer)) > 0) {
+						jos.write(buffer, 0, len);
+					}
+					jos.closeEntry();
+				} catch (FileNotFoundException e) {
+
+				}
+
+			}
+		} catch (IOException e) {
+			System.err.println(e + "Ошибка доступа");
+		} catch (IllegalArgumentException e) {
+			System.err.println(e + "Некорректный аргумент");
 		}
 	};
+
 }
