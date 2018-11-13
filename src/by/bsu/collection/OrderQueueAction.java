0cbf6accfd0ae3ff0b0a544763b76e36033e4eb5
oljh <34842956+oljh@users.noreply.github.com>
2018-11-14 00:35:31 +0300

    queue

---
+++ src/by/bsu/collection/OrderQueueAction.java
@@ -0,0 +1,31 @@
+package by.bsu.collection;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class OrderQueueAction {
+	public static void main(String args) {
+		LinkedList<Order> orders = new LinkedList<Order>() {
+			{
+				add(new Order(231, 12.f));
+				add(new Order(389, 2.9f));
+				add(new Order(217, 1.7f));
+			}
+		};
+		Queue<Order> queueA = orders;
+		queueA.offer(new Order(222, 9.7f));
+		orderProcessing(queueA); // обработка очереди
+		if (queueA.isEmpty()) {
+			System.out.println("Queue of Orders is empty");
+		}
+	}
+
+	public static void orderProcessing(Queue<Order> queue) {
+		Order ob = null;
+		//заменить while --> do{} while
+		 while ((ob = queue.poll()) != null) 
+			 {
+			System.out.println("Order # "+ ob.getOrderId()+"");
+		}
+	}
+}
