65466f557b8291b0b29b9719a4d5e237a33709e6
sapp <sapp@192.168.21.191>
2018-11-20 17:00:43 +0300

    hash

--- src/by/bsu/legacy/HashTableDemo.java
+++ src/by/bsu/legacy/HashTableDemo.java
@@ -6,7 +6,7 @@ public class HashTableDemo {
     public static void main(String[] args){
         Hashtable<Integer, Double> ht = new Hashtable<Integer, Double>(){
             {
-                for(int i=0; i<5; i++) ht.put(i, Math.atan(i));
+                for(int i=0; i<5; i++) { ht.put(i, Math.atan(i));}
             }
         };
     }
