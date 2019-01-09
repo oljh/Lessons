ee6b08b78e43cd0d346abace950915a7cc986491
sapp <sapp@192.168.21.191>
2019-01-09 17:25:23 +0300

    synchr

---
+++ src/by/bsu/learning/DoubleResource.java
@@ -0,0 +1,42 @@
+package by.bsu.learning;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class DoubleResource {
+	private Deque<String> list = new LinkedList<String>();
+	private Lock lock = new ReentrantLock();
+	private Condition isFree = lock.newCondition();
+
+	public void adding(String str, int i) {
+		try {
+			lock.lock();
+			list.addLast(i + "<" + str);
+			TimeUnit.MILLISECONDS.sleep(new Random().nextInt(50));
+			list.add(i + ">" + i);
+			isFree.signal();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public String deleting() {
+		lock.lock();
+		String s = list.poll();
+		s += list.poll();
+		isFree.signal();
+		lock.unlock();
+		return s;
+	}
+
+	public String toString() {
+		return list.toString();
+	}
+}
