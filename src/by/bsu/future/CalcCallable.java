1f96787ed7fa92c324183462df5a85e8cf3c938c
sapp <sapp@192.168.21.191>
2019-01-15 16:47:36 +0300

    callable

---
+++ src/by/bsu/future/CalcCallable.java
@@ -0,0 +1,12 @@
+package by.bsu.future;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+public class CalcCallable implements Callable<Number> {
+	@Override
+	public Number call() throws Exception {
+		Number res = new Random().nextGaussian(); // имитация вычислений
+		return res;
+	}
+}
---
+++ src/by/bsu/future/ProductList.java
@@ -0,0 +1,18 @@
+package by.bsu.future;
+
+import java.util.ArrayDeque;
+
+public class ProductList {
+	private static ArrayDeque<String> arr = new ArrayDeque<String>() {
+		{
+			this.add("Product 1");
+			this.add("Product 2");
+			this.add("Product 3");
+			this.add("Product 4");
+			this.add("Product 5");
+		}
+	};
+	public static String getProduct() {
+		return arr.poll();
+	}
+}
